6b8efb08fd26dc36e37de728c6e0f2944ef7d9b2
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2017-05-14 17:33:38 -0300

    Change marker to bounce once

--- README.md
+++ README.md
@@ -10,11 +10,15 @@ https://jquery.com/
 
 Google Maps API
 
+Useful for markers
 https://developers.google.com/maps/documentation/javascript/markers
 
 Useful for timeouts and drop animation
 https://developers.google.com/maps/documentation/javascript/examples/marker-animations-iteration
+http://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
 
+Info windows:
+https://developers.google.com/maps/documentation/javascript/infowindows
 
 
 bootstrap-4.0.0-alpha.6-dist
--- js/neimap.js
+++ js/neimap.js
@@ -21,6 +21,8 @@ function GoogleError() {
 function PointOfInterestViewModel() {
     var self = this;
     var map;
+    self.openInfowindow;
+
     self.mapCenter = {lat: -33.10, lng: -68.85};
 
     // Point of Interest data - TODO: load POI's from server via JSON
@@ -74,29 +76,66 @@ function PointOfInterestViewModel() {
         var marker = new google.maps.Marker({
             map: map,
             position: location,
-            animation: google.maps.Animation.DROP,
-            // title appears as tooltip on map
-            title: self.pointsOfInterest[i].name
+            // title appears as a tooltip on map
+            title: self.pointsOfInterest[i].name,
+            // Set first character of name as marker icon
+            label: self.pointsOfInterest[i].name.charAt(0),
         });
 
-        self.markers.push(marker);
+        self.markers.push(
+            {
+                marker: marker,
+                id: i,
+            }
+        );
 
         // google.maps.event.addListener(self.markers[i],'click', function(){
-        self.markers[i].addListener('click', function(){
-            if (this.getAnimation() !== null) {
-                this.setAnimation(null);
-            } else {
-                this.setAnimation(google.maps.Animation.DROP);
-            }
+        self.markers[i].marker.addListener('click', function(){
+            bouncingMarker = this
+
+            var label = bouncingMarker.getLabel();
+            // labels do not bounce with marker so remove it temporarely
+            bouncingMarker.setLabel(' ');
+            // Now let the marker bounce
+            bouncingMarker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function(){ bouncingMarker.setAnimation(null); }, 750);
+            // Now restore the original label
+            setTimeout(function(){ bouncingMarker.setLabel(label); }, 800);
+
+            var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            self.openInfowindow = infowindow.open(map, this);
         });
 
 
         self.pointOfInterestLinks.push(
-            new PointOfInterest(i, self.pointsOfInterest[i], self.markers[i])
+            new PointOfInterest(i, self.pointsOfInterest[i], self.markers[i].marker)
         );
 
         // Add marker to map
-        self.markers[i].setMap(map);
+        self.markers[i].marker.setMap(map);
     }
 
     // Click on link
