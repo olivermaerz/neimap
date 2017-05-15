8ba160e3b077d3edc4360ca98013399e84e1283d
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2017-05-15 17:28:59 -0300

    Reorganize the ajax call, add data for bodegas

--- js/neimap.js
+++ js/neimap.js
@@ -5,31 +5,35 @@ var pointsOfInterest = [
         lat: -32.9700846,
         lng: -68.5675192,
         foursquare: '543412cb498e3f8059825cec',
-        altDescription: 'Zuccardi main winery. Great Malbec and Bonarda',
+        altDescription: 'Zuccardi winery in Maipu. Great Malbec and Bonarda',
     },
     {
-        name: 'Zuccardi Valle de Uco',
-        lat: -32.9465165,
-        lng: -68.8623348,
-        altDescription: 'Zuccardi Valle de Uco winery',
+        name: 'Lopez',
+        lat: 32.962649,
+        lng: -68.786521,
+        foursquare: '4c7dae7701589521ccaf0363',
+        altDescription: 'Lopez winery in Maipu. Good Malbecs. ',
     },
     {
         name: 'Salentein',
         lat: -33.2863936,
         lng: -69.1479992,
-        altDescription: 'Salentein winery. Great reds.',
+        foursquare: '4c6951eb35d3be9af2f21e06',
+        altDescription: 'Salentein winery in Tunuyán. Great reds.',
     },
     {
         name: 'SinFin',
         lat: -32.9755586,
         lng: -68.7014326,
+        foursquare: '52053f7704931e0e1ac82125',
         altDescription: 'SinFin winery. Good Malbecs and a great Grappa.',
     },
     {
         name: 'Vistandes',
         lat: -33.0246466,
         lng: -68.7715359,
-        altDescription: 'Vistandes winery. Good wines',
+        foursquare: '4d8f4c741716a143e2ce46f7',
+        altDescription: 'Vistandes winery in Maipu. Popular place for winetasting.',
     }
 ];
 
@@ -44,7 +48,7 @@ var foursquare = {
 // One row in the sidebar menu
 function PointOfInterest(id, pointOfInterest, marker) {
     var self = this;
-    self.id = id;
+    id = id;
     self.pointOfInterest = ko.observable(pointOfInterest);
     self.marker = marker;
 }
@@ -52,34 +56,78 @@ function PointOfInterest(id, pointOfInterest, marker) {
 
 function GoogleError() {
     // Could not load Google Maps library show user a message
+    alert('Could not connect to Google Maps. No Internet???');
 }
 
 //  Viewmodel
 function PointOfInterestViewModel() {
     var self = this;
     self.map;
-    self.infowindow = {};
-
+    infowindow = {};
     self.mapCenter = {lat: -33.10, lng: -68.85};
+    //self.pointsOfInterest = pointsOfInterest;
 
-    self.venue = ko.observableArray([]);
+    // Array for venue (JSON response data)
+    //self.venue = ko.observableArray();
 
+    // Array to hold links for sidebar menu
+    self.pointOfInterestLinks = ko.observableArray();
 
+    // Object to hold content for info window
+    var Content = function (html) {
+        this.html = html;
+    };
 
+    // Center map
     self.map = new google.maps.Map(document.getElementById('map'), {
         center: self.mapCenter,
         zoom: 10
     });
 
-    // Editable data
-    self.pointOfInterestLinks = ko.observableArray();
-
     // Array to push markers
     self.markers = [];
 
     // Loop over array and set markers
-    for (var i = 0, len = pointsOfInterest.length; i < len; i ++) {
+    for (var i = 0, len = pointsOfInterest.length; i < len ; i ++) {
         var location = {lat: pointsOfInterest[i].lat, lng: pointsOfInterest[i].lng};
+
+        //contentString = new Content('Dummy');
+        self.contentFoursquare = ko.observable();
+        self.contentFoursquareError = ko.observable();
+
+        // Retreive data about venue from foursquare API in JSON format
+        $.getJSON('https://api.foursquare.com/v2/venues/' + pointsOfInterest[i].foursquare + '?v=20170101&client_id=L5FM2XQTT5NNJO44NA1M1P1PCP4YYICJZQBMNDAV2GNOPNIZ&client_secret=1ZIPF4L3JHYAWYUGLDXLCX25RKSMXEJYMZEBZQKQ32ZX4KTO', function(allData) {
+            var mappedVenue = $.map(allData.response, function(item) { return new Venue(item) });
+
+            var test = 123;
+
+            var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">' + mappedVenue[0].name + '</h1>' +
+                '<div id="bodyContent">'+
+                '<p>' + mappedVenue[0].address + '</p>'+
+                '<p>Attribution: Information about venue by <a href="https://www.foursquare.com/'+
+                '">Foursquare</a>. Map data from <a href="https://maps.google.com">Google Maps</a>.'+
+                '</p>'+
+                '<div class="image-list">';
+
+
+            // Add images from photos array
+            for (var j = 0, len = mappedVenue[0].photos.length; (j < len) && (j < 2); j++) {
+                contentString += '<img src="'+ mappedVenue[0].photos[j].prefix +'100x100'+ mappedVenue[0].photos[j].suffix +'"/> ';
+            }
+
+            contentString +=
+                '</div>'+
+                '</div>'+
+                '</div>';
+                self.contentFoursquare(contentString);
+
+        }).fail(function() {
+            self.contentFoursquareError('Could not load additional info. Internet down?');
+        });
+
         var marker = new google.maps.Marker({
             map: self.map,
             position: location,
@@ -102,7 +150,7 @@ function PointOfInterestViewModel() {
         self.markers[i].marker.addListener('click', function(){
             var bouncingMarker = this
 
-            //self.map.setCenter(bouncingMarker.position);
+
 
             var label = bouncingMarker.title.charAt(0);
             // labels do not bounce with marker so remove it temporarely
@@ -116,33 +164,21 @@ function PointOfInterestViewModel() {
                 }, 650);
 
 
-            if(self.infowindow.open) {
-                self.infowindow.close();
+            if(infowindow.open) {
+                infowindow.close();
             }
 
-            var contentString = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h1 id="firstHeading" class="firstHeading">' + bouncingMarker.title + '</h1>' +
-                '<div id="bodyContent"><p>' + bouncingMarker.content + '</p>'+
-                //'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                //'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-                //'</p>'+
-                '<ul data-bind="foreach: venue, visible: venue().length > 0">'+
-                '<li data-bind="value: name"/>'+
-                '</li>'+
-                '</ul>'+
-                '</div>'+
-                '</div>';
 
-            self.infowindow = new google.maps.InfoWindow({
-                content: contentString,
+            infowindow = new google.maps.InfoWindow({
+                content: self.contentFoursquare(),
             });
 
             // Recenter the map
             self.map.setCenter(new google.maps.LatLng(bouncingMarker.position.lat(), bouncingMarker.position.lng()));
             // Open the info window
-            self.infowindow.open(self.map, bouncingMarker);
+            infowindow.open(self.map, bouncingMarker);
+
+
         });
 
 
@@ -153,10 +189,7 @@ function PointOfInterestViewModel() {
         // Add marker to map
         self.markers[i].marker.setMap(self.map);
 
-        $.getJSON("https://api.foursquare.com/v2/venues/543412cb498e3f8059825cec?v=20170101&client_id=L5FM2XQTT5NNJO44NA1M1P1PCP4YYICJZQBMNDAV2GNOPNIZ&client_secret=1ZIPF4L3JHYAWYUGLDXLCX25RKSMXEJYMZEBZQKQ32ZX4KTO", function(allData) {
-            var mappedVenue = $.map(allData.response, function(item) { return new Venue(item) });
-            self.venue(mappedVenue);
-        });
+
 
 
     }
@@ -168,7 +201,10 @@ function PointOfInterestViewModel() {
 }
 
 function Venue(data) {
-    this.name = ko.observable(data.name);
+    this.name = data.name;
+    // formattedAddress is array - join elements with a dash into a string
+    this.address = data.location.formattedAddress.join(' - ');
+    this.photos = data.photos.groups[0].items;
 }
 
 
